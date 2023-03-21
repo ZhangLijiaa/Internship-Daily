5d4989d152966fc071c679a1c775dbc72bce3588
ZhangLijia <2834636337@qq.com>
2023-03-21 21:53:14 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -23,48 +23,6 @@ function App() {
     { name: '开发任务-5', time: '22-05-22 18:11' }
   ]);
 
-  const map1 = {
-    pending:{
-      deleteTabOperation:setPendingList,
-      fromTab:pendingList,
-      addTabOperation:setDoingList,
-      toTab:doingList
-    },
-    doing:{
-      deleteTabOperation:setDoingList,
-      fromTab:doingList,
-      addTabOperation:setPendingList,
-      toTab:pendingList
-    },
-    completed:{
-      deleteTabOperation:setCompletedList,
-      fromTab:completedList,
-      addTabOperation:setPendingList,
-      toTab:pendingList
-    }
-  }
-
-  const map2 = {
-    pending:{//操作标识符
-      deleteTabOperation:setPendingList,//删除数据列表
-      fromTab:pendingList,//要被删除的数据来源
-      addTabOperation:setCompletedList,//添加数据列表
-      toTab:completedList//要被添加的数据列表
-    },
-    doing:{
-      deleteTabOperation:setDoingList,
-      fromTab:doingList,
-      addTabOperation:setCompletedList,
-      toTab:completedList
-    },
-    completed:{
-      deleteTabOperation:setCompletedList,
-      fromTab:completedList,
-      addTabOperation:setDoingList,
-      toTab:doingList
-    }
-  }
-
   const click = (e) => {
     setShow(true)
   }
@@ -76,9 +34,8 @@ function App() {
     setShow(false)
   }
 
-  const firstToggle = (type, name, map) => {
+  const firstToggle = (type, name, deleteTabOperation, fromTab, addTabOperation, toTab) => {
     //把map里的数据解构赋值
-    const {deleteTabOperation, fromTab, addTabOperation, toTab} = map[type]
     //删除fromTab的数据
     deleteTabOperation(fromTab.filter((element, index, array) => {
       return element.name !== name
@@ -105,15 +62,15 @@ function App() {
             </>
           }>
             { show ? <AddBoardCard onSubmit={submit} /> : ''}
-            {pendingList.map(item => <BoardCard key={item.name} {...item} type='pending'  firstToggle={firstToggle} map1={map1} map2={map2}/>)}
+            {pendingList.map(item => <BoardCard key={item.name} {...item} type='pending'  firstToggle={firstToggle} remove={setPendingList} add1={setDoingList} deleteData={pendingList} addData1={doingList} add2={setCompletedList}  addData2={completedList}/>)}
         </BoardColumn>
 
         <BoardColumn className="column-doing" name="处理中">
-          { doingList.map(item => <BoardCard key={item.name} {...item} type='doing'  firstToggle={firstToggle} map1={map1} map2={map2}/>) }
+          { doingList.map(item => <BoardCard key={item.name} {...item} type='doing'  firstToggle={firstToggle} remove={setDoingList} add1={setPendingList} deleteData={doingList} addData1={pendingList} add2={setCompletedList}  addData2={completedList}/>)}
         </BoardColumn>
 
         <BoardColumn className="column-completed" name="已完成">
-          { completedList.map(item => <BoardCard key={item.name} {...item} type='completed' firstToggle={firstToggle} map1={map1} map2={map2}/>) }
+          { completedList.map(item => <BoardCard key={item.name} {...item} type='completed' firstToggle={firstToggle} remove={setCompletedList} add1={setPendingList} deleteData={completedList} addData1={pendingList} add2={setDoingList}  addData2={doingList}/>)}
         </BoardColumn>
       </Board>
     </div >
--- src/BoardCard.js
+++ src/BoardCard.js
@@ -1,14 +1,14 @@
 import React from "react";
-const BoardCard = ({ name, time, firstToggle, type, map1, map2 }) => {
+const BoardCard = ({ name, time, firstToggle, type, remove, add1, deleteData, addData1, add2, addData2 }) => {
   return (
     <li className='card'>
       <div className='card-name'>{name}</div>
       <div className='card-time'>{time}</div>
       <button onClick={() => {
-          firstToggle(type, name, map1)
+          firstToggle(type, name, remove, deleteData, add1, addData1)
       }}>第一个</button>
       <button onClick={() => {
-          firstToggle(type, name, map2)
+          firstToggle(type, name, remove, deleteData, add2, addData2)
       }}>第二个</button>
     </li>
   );
